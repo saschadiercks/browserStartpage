b8a419e027ebe7b21cd6d4dc1e7ff691cc276e19
Sascha Diercks <sascha.diercks@googlemail.com>
2017-01-05 17:47:29 +0100

    uglifyJS

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,20 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
 
 
 
 // Place Code for tasks here
+
+// SCSS
 gulp.task('sass', function () {
 	return gulp.src('./src/scss/**/*.scss')
 	.pipe(sourcemaps.init())
 	.pipe(sass().on('error', sass.logError))
 	.pipe(autoprefixer({
-		browsers: ['last 2 versions'],
+		browsers: ['last 2 versions','>5%'],
 		cascade: false
 	}))
 	.pipe(sourcemaps.write('.'))
@@ -21,6 +25,19 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
 	gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+// JS
+gulp.task('compress', function (cb) {
+	var options = {
+		mangle: 'false'
+	};
+
+	pump([
+		gulp.src('./src/js/*.js'),
+		uglify(),
+		gulp.dest('./htdocs/assets/js')
+	],cb);
 });
 
 gulp.task('default', function() {
