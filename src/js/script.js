16ca5a6a7314b352cf14526571324d7a11cccf57
Sascha Diercks <sascha.diercks@googlemail.com>
2017-01-31 09:57:48 +0100

    prepare for automation

---
+++ src/js/script.js
@@ -0,0 +1,52 @@
+// TODO: use data-target instead of elementIndex
+
+// is the DOM ready for manipulation?
+document.addEventListener('DOMContentLoaded', function() {
+
+	// add JS to body-tag to allow CSS-Manipulation if JS is available
+	document.getElementsByTagName("body")[0].className += "js";
+
+	// initially set Element-count of tabs and tabbedContent and check consistency
+	var tabTrigger = document.getElementsByClassName("tab");
+	var tabbedContent = document.getElementsByClassName("tabbed-content");
+	if(tabTrigger.length !== tabbedContent.length) {
+		console.log("count of tabs and tabbed-contend isn't consistent");
+	}
+
+	// Tab-Handling-Function
+	// -- set Current-Tab and tabbed-content
+	var setCurrentTab = function(newTabId) {
+		markTabTrigger = document.getElementsByClassName("tab")[newTabId].classList.add("active");
+		unhideSelectedContent = document.getElementsByClassName("tabbed-content")[newTabId].classList.add("active");
+		localStorage.setItem("tabbedContentId", newTabId);
+		console.log("localStorage ID is: " + newTabId);
+	}
+
+	// -- unset Current-Tab and tabbed-conent
+	var unsetTabs = function() {
+		for(i=0; i < tabbedContent.length; i++) {
+			tabTrigger[i].classList.remove("active");
+			tabbedContent[i].classList.remove("active");
+		}
+	}
+
+	// -- if DOM is ready, check if localStorage is filled
+	var savedLocalStorageId = localStorage.getItem("tabbedContentId");
+	if(savedLocalStorageId !== null) {
+		setCurrentTab(savedLocalStorageId);
+	} else {
+		console.log("localStorag is empty, setting default");
+		setCurrentTab(0);
+	}
+
+	// -- switch tabs onClick
+	for(i=0; i < tabTrigger.length; i++) {
+		(function(index){
+			tabTrigger[i].onclick = function(){
+				unsetTabs();
+				setCurrentTab(index);
+				return false;
+			}
+		})(i);
+	}
+});
