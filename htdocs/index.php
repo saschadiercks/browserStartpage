ba726e9145c6c4f3f8bc833da52f681950780e75
Sascha Diercks <sascha.diercks@googlemail.com>
2017-06-21 23:23:44 +0200

    basicparsing of json with php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,31 @@
 <?php
 
+	// Set Environemt
+	$dataUrl = "data/data.json";				// Set the url to retrieve the data from
+	$content = file_get_contents($dataUrl);		// Get the data
+	$json = json_decode($content, true);		// (true) returns the json as array-structure
+
+
+
+	// Output
+	function buildTabs($array) {
+		$keys = array_keys($array);
+		foreach($keys as $key) {
+			echo $key;
+		}
+	}
+
+	function buildContent($array) {
+		foreach($array as $item) {
+			foreach($item as $contentItem) {
+				if(isset($contentItem['url'])) { echo $contentItem['url']."<br/>"; }
+				if(isset($contentItem['image'])) { echo $contentItem['image']."<br/>"; }
+				if(isset($contentItem['title'])) { echo $contentItem['title']."<br/>"; }
+			}
+		}
+	}
+
+	buildTabs($json['content']); echo "<br/>";
+	buildContent($json['content']); echo "<br/>";
+	buildContent($json['footer']); echo "<br/>";
 ?>
