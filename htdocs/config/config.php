64f1f766dea8d1fe6d2da80e0e651e6ef9e83c2c
Sascha Diercks <sascha@metaideen.de>
2018-11-05 20:15:26 +0100

    add possibility to configure link-target in json

--- htdocs/config/config.php
+++ htdocs/config/config.php
@@ -16,7 +16,7 @@
 	// Set Environemt
 	$cssUrl = 'assets/css/site.css';				// Set the url to retreive the css from
 	$jsUrl = 'assets/js/script.js';					// Set the url to retrieve the js from
-	$manifestUrl = 'application.manifest.php';		// This is the name of the manifest-file
+	//$manifestUrl = 'application.manifest.php';		// This is the name of the manifest-file
 
 	// Set php requirements
 	// none
@@ -35,4 +35,5 @@
 	$content = $json['content'];				// Get content-array directly
 	$bookmarks = $json['bookmarks'];			// Get bookmarks-array
 	$footer = $json['footer'];					// Get footer-array
+	$linktarget = $json['linktarget'];			// Set linkTarget
 ?>
