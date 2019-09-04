3b7dbbc7dffe0c39c9a196e5e0d86c9645e8ebe8
Sascha Diercks <sascha@saschadiercks.de>
2019-09-04 22:43:10 +0200

    rename webpack

--- _tasks/scripts.js
+++ _tasks/scripts.js
@@ -2,15 +2,15 @@
 const config = require('./_config.json')
 const gulp = require('gulp')
 const uglify = require('gulp-uglify')
-const webpackStream = require('webpack-stream')
+const webpack = require('webpack-stream')
 
 /* ################# */
 /* ##### Tasks ##### */
 /* ################# */
 gulp.task('scripts:build', function () {
 	return gulp.src(config.assetSrc + '/js/')
-	.pipe(webpackStream({
-		config : require('../webpack.config.js')
+	.pipe(webpack({
+		config : require('../webpack.config.js'),
 	}))
 	//.pipe(uglify())
 	.pipe(gulp.dest('.'));
