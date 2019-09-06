cf8f391dfdd75126dd077cc58feb98b158f3ac26
Sascha Diercks <sascha@saschadiercks.de>
2019-09-06 23:43:44 +0200

    small js-components

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -7,22 +7,9 @@ import stickyElement from "./functions/stickyElement.js";
 import fixScrollPos from "./functions/fixScrollPos.js";
 import handleTriggers from "./functions/handleTriggers.js";
 
-
-//import addClass from "./functions/addClass.js";
-//import findAll from "./functions/findAll.js";
-//import doLocalStorage from "./functions/localStorage.js";
-//import removeClass from "./functions/removeClass.js";
-//import scrollToPos from "./functions/scrollToPos.js";
-//import tabHandling from "./components/tabHandling.js";
-
 // ####################
 // ##### settings #####
 // ####################
-// const selector__applyJsClassTo = "body";
-// const selector__notification = ".notification";
-// const localStorage__idTab = "currentTab";
-// const selector__tabContent = ".tabbed-content";
-// const class__isActive = "sdi-js-active";
 
 // ###### script ######
 // is the DOM ready for manipulation?
@@ -35,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	handleTabs('.js-tab-trigger','.tabbed-content');
 
 	// handle triggers
-	handleTriggers('.js-flyout-trigger', false);
+	handleTriggers('.js-flyout-trigger', fixScrollPos);
 	handleTriggers('.js-collapse-trigger', false);
 	handleTriggers('.js-modal-trigger', fixScrollPos);
 
@@ -45,20 +32,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// --- Show/hide notification
 	notificationKeydown(".notification");
-
-
-
-	// // ##### handle localeStorage
-	// // get Id from first tab to save as fallback
-	// const id__firstTab = '#' + findAll(selector__tabContent)[0].id;
-
-	// // show saved tab
-	// if(doLocalStorage(localStorage__idTab) === null) {
-	// 	// show first tab
-	// 	doLocalStorage(localStorage__idTab, id__firstTab);
-	// 	addClass(findAll(id__firstTab), class__isActive);
-	// } else {
-	// 	//show saved tab
-	// 	addClass(findAll(doLocalStorage(localStorage__idTab)), class__isActive);
-	// }
 });
