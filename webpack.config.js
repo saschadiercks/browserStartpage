3618c2c0b1cb045c46350896f0fd94c1784f2451
Sascha Diercks <sascha@saschadiercks.de>
2020-05-12 22:55:49 +0200

    integrate prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
 /* #### Setting #### */
-const config = require('./_tasks/_config.json')
+const config = require("./_tasks/_config.json");
 
 /* ################# */
 /* ##### Tasks ##### */
 /* ################# */
 module.exports = {
-	entry: {
-		site: [
-			`./${config.assetSrc}/js/site.js`
-		]
-	},
-	output: {
-		filename: `./${config.assetDist}/js/[name].js`
-	},
-	mode: process.env.NODE_ENV
+  entry: {
+    site: [`./${config.assetSrc}/js/site.js`]
+  },
+  output: {
+    filename: `./${config.assetDist}/js/[name].js`
+  }
 };
