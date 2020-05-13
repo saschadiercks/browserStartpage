e177a9f81bdab109bbfa21283e3ede37c420011e
Sascha Diercks <sascha@saschadiercks.de>
2020-05-13 19:36:53 +0200

    prepare theming

--- htdocs/assets/js/site.js
+++ htdocs/assets/js/site.js
@@ -1,1 +1,474 @@
-!function(t){var e={};function n(c){if(e[c])return e[c].exports;var a=e[c]={i:c,l:!1,exports:{}};return t[c].call(a.exports,a,a.exports,n),a.l=!0,a.exports}n.m=t,n.c=e,n.d=function(t,e,c){n.o(t,e)||Object.defineProperty(t,e,{configurable:!1,enumerable:!0,get:c})},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=3)}([function(t,e,n){"use strict";e.a=function(t){return document.querySelectorAll(t)}},function(t,e,n){"use strict";e.a=function(t,e){t.forEach(function(t){t.classList.add(e)})}},function(t,e,n){"use strict";e.a=function(t,e){t.forEach(function(t){t.classList.remove(e)})}},function(t,e,n){t.exports=n(4)},function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var c=n(5),a=n(6),o=n(7),i=n(8),r=n(10),s=n(11);document.addEventListener("DOMContentLoaded",function(){Object(c.a)("body"),Object(o.a)(".js-tab-trigger",".tabbed-content"),Object(s.a)(".js-flyout-trigger",r.a),Object(s.a)(".js-collapse-trigger",!1),Object(s.a)(".js-modal-trigger",r.a),Object(i.a)("#application-footer","#content","padding-bottom"),Object(a.a)(".notification")})},function(t,e,n){"use strict";e.a=function(t){(t=Object(c.a)(t)).forEach(function(){Object(a.a)(t,i),Object(o.a)(t,r)})};var c=n(0),a=n(1),o=n(2);const i="js",r="no-js"},function(t,e,n){"use strict";e.a=function(t){var e=Object(c.a)(t);document.addEventListener("keydown",function(){e.forEach(function(){Object(a.a)(e,r),Object(o.a)(e,i)})}),document.addEventListener("keyup",function(){e.forEach(function(){Object(a.a)(e,i),Object(o.a)(e,r)})})};var c=n(0),a=n(1),o=n(2);const i="js-hidden",r="js-visible"},function(t,e,n){"use strict";e.a=function(t,e){var n=Object(c.a)(t),s=Object(c.a)(e);if(document.location.hash&&Object(c.a)('a[data-target="'+document.location.hash+'"]').length>0)f(document.location.hash),window.scrollTo(0,0);else{var u=localStorage.getItem(r);null!==u?f(u):(document.querySelector(t).classList.add(i),document.querySelector(e).classList.add(i))}function f(t){Object(o.a)(n,i),Object(o.a)(s,i),Object(a.a)(Object(c.a)(t),i),Object(a.a)(Object(c.a)('a[data-target="'+t+'"]'),i)}function l(t){!0!==event.altKey&&localStorage.setItem(r,t)}n.forEach(function(t){t.addEventListener("click",function(t){t.preventDefault();var e=this.getAttribute("data-target");f(e),l(e),history.pushState(null,null,e)})})};var c=n(0),a=n(1),o=n(2);const i="sdi-is-active",r="lastTabID"},function(t,e,n){"use strict";e.a=function(t,e,n){var r=Object(o.a)(t),s=Object(a.a)(t).clientHeight+"px";Object(c.a)(r,i),Object(a.a)(e).style.setProperty(n,s)};var c=n(1),a=n(9),o=n(0);const i="js-sticky"},function(t,e,n){"use strict";e.a=function(t){return document.querySelector(t)}},function(t,e,n){"use strict";e.a=function(t){var e=window.pageYOffset;c.classList.contains(a)?(c.classList.remove(a),c.style.top="",window.scrollTo(0,o)):(c.classList.add(a),c.style.top="-"+e+"px",o=e)};const c=document.querySelector("body"),a="sdi-is-fixed";var o},function(t,e,n){"use strict";e.a=function(t,e){Object(c.a)(t).forEach(function(t){t.addEventListener("click",function(){var t=Object(c.a)(this.getAttribute("data-target"));Object(a.a)(t,o),"function"==typeof e&&e(),event.preventDefault()})})};var c=n(0),a=n(12);const o="js-is-active"},function(t,e,n){"use strict";e.a=function(t,e){t.forEach(function(t){t.classList.toggle(e)})}}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = findAll;
+// ###### import ######
+
+// ####################
+// ##### settings #####
+// ####################
+
+// ###### script ######
+function findAll(selector) {
+	return document.querySelectorAll(selector);
+}
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = addClass;
+// ###### import ######
+
+// ####################
+// ##### settings #####
+// ####################
+
+// ###### script ######
+function addClass(elements, className) {
+	elements.forEach(function(element){
+		element.classList.add(className);
+	});
+}
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = removeClass;
+// ###### import ######
+
+// ####################
+// ##### settings #####
+// ####################
+
+// ###### script ######
+function removeClass(elements,className) {
+	elements.forEach(function(element){
+		element.classList.remove(className);
+	});
+}
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(4);
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__components_setJsAvailability_js__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__components_notificationKeydown_js__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__components_handleTabsLocalStorage_js__ = __webpack_require__(7);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__functions_stickyElement_js__ = __webpack_require__(8);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__functions_fixScrollPos_js__ = __webpack_require__(10);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__functions_handleTriggers_js__ = __webpack_require__(11);
+// ###### import ######
+
+
+
+
+
+
+
+
+// ####################
+// ##### settings #####
+// ####################
+
+// ###### script ######
+// is the DOM ready for manipulation?
+document.addEventListener('DOMContentLoaded', function() {
+
+	// --- Toggle JS Availability
+	Object(__WEBPACK_IMPORTED_MODULE_0__components_setJsAvailability_js__["a" /* default */])("body");
+
+	// handle tabs
+	Object(__WEBPACK_IMPORTED_MODULE_2__components_handleTabsLocalStorage_js__["a" /* default */])('.js-tab-trigger','.tabbed-content');
+
+	// handle triggers
+	Object(__WEBPACK_IMPORTED_MODULE_5__functions_handleTriggers_js__["a" /* default */])('.js-flyout-trigger', __WEBPACK_IMPORTED_MODULE_4__functions_fixScrollPos_js__["a" /* default */]);
+	Object(__WEBPACK_IMPORTED_MODULE_5__functions_handleTriggers_js__["a" /* default */])('.js-collapse-trigger', false);
+	Object(__WEBPACK_IMPORTED_MODULE_5__functions_handleTriggers_js__["a" /* default */])('.js-modal-trigger', __WEBPACK_IMPORTED_MODULE_4__functions_fixScrollPos_js__["a" /* default */]);
+
+	// -- make elements sticky
+	Object(__WEBPACK_IMPORTED_MODULE_3__functions_stickyElement_js__["a" /* default */])('#application-footer','#content','padding-bottom');
+
+	// --- Show/hide notification
+	Object(__WEBPACK_IMPORTED_MODULE_1__components_notificationKeydown_js__["a" /* default */])(".notification");
+});
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = setJsAvailability;
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__functions_findAll_js__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__functions_addClass_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__functions_removeClass_js__ = __webpack_require__(2);
+// ###### import ######
+
+
+
+
+// ####################
+// ##### settings #####
+// ####################
+const class__jsIsAvailable = 'js';
+const class__jsIsNotAvailable = 'no-js';
+
+// ###### script ######
+function setJsAvailability(selector) {
+	var selector = Object(__WEBPACK_IMPORTED_MODULE_0__functions_findAll_js__["a" /* default */])(selector);
+
+	selector.forEach(function() {
+		Object(__WEBPACK_IMPORTED_MODULE_1__functions_addClass_js__["a" /* default */])(selector, class__jsIsAvailable);
+		Object(__WEBPACK_IMPORTED_MODULE_2__functions_removeClass_js__["a" /* default */])(selector, class__jsIsNotAvailable);
+	});
+}
+
+
+/***/ }),
+/* 6 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = notificationKeyDown;
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__functions_findAll_js__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__functions_addClass_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__functions_removeClass_js__ = __webpack_require__(2);
+// ###### import ######
+
+
+
+
+// ####################
+// ##### settings #####
+// ####################
+const class__isHidden = 'js-hidden';
+const class__isVisible = 'js-visible';
+
+// ###### script ######
+function notificationKeyDown(selector) {
+	var targetElement = Object(__WEBPACK_IMPORTED_MODULE_0__functions_findAll_js__["a" /* default */])(selector);
+
+	document.addEventListener('keydown', function() {
+		targetElement.forEach( function() {
+			Object(__WEBPACK_IMPORTED_MODULE_1__functions_addClass_js__["a" /* default */])(targetElement, class__isVisible);
+			Object(__WEBPACK_IMPORTED_MODULE_2__functions_removeClass_js__["a" /* default */])(targetElement, class__isHidden);
+		});
+	});
+
+	document.addEventListener('keyup', function() {
+		targetElement.forEach( function() {
+			Object(__WEBPACK_IMPORTED_MODULE_1__functions_addClass_js__["a" /* default */])(targetElement, class__isHidden);
+			Object(__WEBPACK_IMPORTED_MODULE_2__functions_removeClass_js__["a" /* default */])(targetElement, class__isVisible);
+		});
+	});
+}
+
+
+/***/ }),
+/* 7 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = tabHandling;
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__functions_findAll_js__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__functions_addClass_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__functions_removeClass_js__ = __webpack_require__(2);
+// ###### import ######
+
+
+
+
+// ####################
+// ##### settings #####
+// ####################
+const class__isActive = 'sdi-is-active';
+const key__localStorage = 'lastTabID';
+
+// ###### script ######
+function tabHandling(selectorTrigger, selectorContent) {
+	var tabTrigger = Object(__WEBPACK_IMPORTED_MODULE_0__functions_findAll_js__["a" /* default */])(selectorTrigger);
+	var tabContent = Object(__WEBPACK_IMPORTED_MODULE_0__functions_findAll_js__["a" /* default */])(selectorContent)
+
+	// -- check for hash and if there is a tab using that hash
+	// to allow linking to tab via hash
+	if (document.location.hash && Object(__WEBPACK_IMPORTED_MODULE_0__functions_findAll_js__["a" /* default */])('a[data-target="' + document.location.hash + '"]').length > 0) {
+		handleTabs(document.location.hash);
+
+		// overwrite scroll-position of hash
+		window.scrollTo(0, 0);
+	} else {
+
+		// -- check local storage
+		var value__localStorage = localStorage.getItem(key__localStorage);
+		if (value__localStorage !== null) {
+			handleTabs(value__localStorage);
+		} else {
+			// wihtout localStorage we'll just show the first tab
+			document.querySelector(selectorTrigger).classList.add(class__isActive);
+			document.querySelector(selectorContent).classList.add(class__isActive);
+		}
+	}
+
+	// -- listen for click on triggers and show/hide content
+	tabTrigger.forEach(function (element) {
+		element.addEventListener('click', function (event) {
+			event.preventDefault();
+
+			var triggerTarget = this.getAttribute('data-target');
+
+			// now handle tabs
+			handleTabs(triggerTarget);
+
+			// save to local storage
+			saveToLocalStorage(triggerTarget);
+
+			// update hash in URL to allow easy copy/paste
+			history.pushState(null, null, triggerTarget);
+		});
+	});
+
+	// -- open tab and mark button as active
+	function handleTabs(selector) {
+
+		// hide all tabs and remove active class from buttons
+		Object(__WEBPACK_IMPORTED_MODULE_2__functions_removeClass_js__["a" /* default */])(tabTrigger, class__isActive);
+		Object(__WEBPACK_IMPORTED_MODULE_2__functions_removeClass_js__["a" /* default */])(tabContent, class__isActive);
+
+		// open saved tab
+		Object(__WEBPACK_IMPORTED_MODULE_1__functions_addClass_js__["a" /* default */])(Object(__WEBPACK_IMPORTED_MODULE_0__functions_findAll_js__["a" /* default */])(selector), class__isActive);
+
+		// add active class to button
+		Object(__WEBPACK_IMPORTED_MODULE_1__functions_addClass_js__["a" /* default */])(Object(__WEBPACK_IMPORTED_MODULE_0__functions_findAll_js__["a" /* default */])('a[data-target="' + selector + '"]'), class__isActive);
+	}
+
+	// -- save to localStorage
+	function saveToLocalStorage(selector) {
+		// save to local storage, when key is not pressed
+		if (event.altKey !== true) {
+			localStorage.setItem(key__localStorage, selector);
+		}
+	}
+}
+
+
+/***/ }),
+/* 8 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = stickyElement;
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__addClass_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__find_js__ = __webpack_require__(9);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__findAll_js__ = __webpack_require__(0);
+// ###### import ######
+
+
+
+
+// ####################
+// ##### settings #####
+// ####################
+const class__sticky = 'js-sticky';
+
+// ###### script ######
+function stickyElement(selectorSticky, selectorCompensate, propertyCompensate) {
+	var stickyElement = Object(__WEBPACK_IMPORTED_MODULE_2__findAll_js__["a" /* default */])(selectorSticky);
+	var stickyHeight = Object(__WEBPACK_IMPORTED_MODULE_1__find_js__["a" /* default */])(selectorSticky).clientHeight + 'px';
+	Object(__WEBPACK_IMPORTED_MODULE_0__addClass_js__["a" /* default */])(stickyElement, class__sticky);
+
+	Object(__WEBPACK_IMPORTED_MODULE_1__find_js__["a" /* default */])(selectorCompensate).style.setProperty(propertyCompensate,stickyHeight);
+}
+
+
+/***/ }),
+/* 9 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = find;
+// ###### import ######
+
+// ####################
+// ##### settings #####
+// ####################
+
+// ###### script ######
+function find(selector) {
+	return document.querySelector(selector);
+}
+
+
+/***/ }),
+/* 10 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = fixScrollPos;
+// ###### import ######
+
+// ####################
+// ##### settings #####
+// ####################
+const selector__body = document.querySelector('body');
+const class__elementIsFixed = 'sdi-is-fixed';
+
+var scrollYSaved;
+
+// ###### script ######
+function fixScrollPos(event) {
+	var scrollY = window.pageYOffset;
+	//console.log(event);
+
+	if(selector__body.classList.contains(class__elementIsFixed)) {
+		selector__body.classList.remove(class__elementIsFixed);
+		selector__body.style.top = '';
+		window.scrollTo(0,scrollYSaved);
+	} else {
+		selector__body.classList.add(class__elementIsFixed);
+		selector__body.style.top = '-' + scrollY + 'px';
+		scrollYSaved = scrollY;
+	}
+}
+
+
+/***/ }),
+/* 11 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = handleTriggers;
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__findAll_js__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__toggleClass_js__ = __webpack_require__(12);
+// ###### import ######
+
+
+
+// ####################
+// ##### settings #####
+// ####################
+const class__isActive = 'js-is-active';
+
+// ###### script ######
+function handleTriggers(selector, callback) {
+	var targetElements = Object(__WEBPACK_IMPORTED_MODULE_0__findAll_js__["a" /* default */])(selector);
+
+	targetElements.forEach(function(element) {
+		element.addEventListener('click', function() {
+			var elementTarget = Object(__WEBPACK_IMPORTED_MODULE_0__findAll_js__["a" /* default */])(this.getAttribute('data-target'));
+			Object(__WEBPACK_IMPORTED_MODULE_1__toggleClass_js__["a" /* default */])(elementTarget, class__isActive);
+
+			// check if a callback is defined
+			if(typeof callback === "function") {
+				callback();
+			}
+			event.preventDefault();
+		});
+	})
+}
+
+
+/***/ }),
+/* 12 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = toggleClass;
+// ###### import ######
+
+// ####################
+// ##### settings #####
+// ####################
+
+// ###### script ######
+function toggleClass(elements, className) {
+	elements.forEach(function(element){
+		element.classList.toggle(className);
+	});
+}
+
+
+/***/ })
+/******/ ]);
