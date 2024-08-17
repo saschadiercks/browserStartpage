ca6f0e766fb3cc80a6e49a7bb49815c532d2cb86
Sascha Diercks <sascha@saschadiercks.de>
2024-08-17 17:19:06 +0200

    some reformatting

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,83 +1,103 @@
 <?php
-header('Content-type: text/html; charset=utf-8');
+header("Content-type: text/html; charset=utf-8");
 
 // Setup
-$projectConfigUrl = 'config/config.php';
-require_once($projectConfigUrl);
+$projectConfigUrl = "config/config.php";
+require_once $projectConfigUrl;
 
-$localhost = array(
-  '127.0.0.1',
-  '::1'
-);
-if (in_array($_SERVER['REMOTE_ADDR'], $localhost)) {
-  $isLocalHost = true;
+$localhost = ["127.0.0.1", "::1"];
+if (in_array($_SERVER["REMOTE_ADDR"], $localhost)) {
+    $isLocalHost = true;
 }
 
 function generateId($value)
 {
-  $needles = array(' ', '/', '.', '#');
-  return strtolower(htmlentities(str_replace($needles, '', $value)));
+    $needles = [" ", "/", ".", "#"];
+    return strtolower(htmlentities(str_replace($needles, "", $value)));
 }
 
 function renderQrModalTrigger($contentItem)
 {
-  if (isset($contentItem['imageQr']) && !empty($contentItem['imageQr']) > 0) {
-    echo '<button class="tile__button js-modal-trigger" data-target="#' . generateId($contentItem['imageQr']) . '">&#9635;</button>';
-  }
+    if (isset($contentItem["imageQr"]) && !empty($contentItem["imageQr"]) > 0) {
+        echo '<button class="tile__button js-modal-trigger" data-target="#' .
+            generateId($contentItem["imageQr"]) .
+            '">&#9635;</button>';
+    }
 }
 
 function returnImage($url, $alt, $class)
 {
-  list($width, $height, $type, $atr) = getimagesize($url);
-  return '<img src="' . $url . '" ' . $atr . ' alt="' . $alt . '" class="' . $class . '" loading="lazy" />';
+    list($width, $height, $type, $atr) = getimagesize($url);
+    return '<img src="' .
+        $url .
+        '" ' .
+        $atr .
+        ' alt="' .
+        $alt .
+        '" class="' .
+        $class .
+        '" loading="lazy" />';
 }
 
 // define linktarget if isset and filled otherwise use default
 if (empty($linktarget)) {
-  $linktarget = "_self";
+    $linktarget = "_self";
 }
 ?>
 
 <!DOCTYPE html>
 <html <?php
-      echo isset($projectLanguage) ? 'lang="' . $projectLanguage . '" ' : FALSE;
-      echo isset($projectDirection) ? 'dir="' . $projectDirection . '" ' : FALSE;
-      echo isset($manifestUrl) && isset($isLocalHost) ? 'manifest="' . $manifestUrl . '" ' : FALSE;
-      echo isset($theme) ? 'class="' . $theme . '" ' : FALSE;
-      ?>>
+echo isset($projectLanguage) ? 'lang="' . $projectLanguage . '" ' : false;
+echo isset($projectDirection) ? 'dir="' . $projectDirection . '" ' : false;
+echo isset($manifestUrl) && isset($isLocalHost)
+    ? 'manifest="' . $manifestUrl . '" '
+    : false;
+echo isset($theme) ? 'class="' . $theme . '" ' : false;
+?>>
 
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <?php
-  echo isset($projectTitle) ? '<title>' . $projectTitle . '</title>' : FALSE;
-  echo isset($projectDescription) ? '<meta name="description" content="' . $projectDescription . '"/>' : FALSE;
-  echo isset($projectKeywords) ? '<meta name="keywords" content="' . $projectKeywords . '"/>' : FALSE;
-  echo isset($projectLanguage) ? '<meta name="language" content="' . $projectLanguage . '"/>' : FALSE;
+  echo isset($projectTitle) ? "<title>" . $projectTitle . "</title>" : false;
+  echo isset($projectDescription)
+      ? '<meta name="description" content="' . $projectDescription . '"/>'
+      : false;
+  echo isset($projectKeywords)
+      ? '<meta name="keywords" content="' . $projectKeywords . '"/>'
+      : false;
+  echo isset($projectLanguage)
+      ? '<meta name="language" content="' . $projectLanguage . '"/>'
+      : false;
   ?>
 
   <!-- mobile scaling -->
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <?php if ($serveAsApplication === TRUE) { ?>
+  <?php if ($serveAsApplication === true) { ?>
     <!-- Website as app -->
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 
     <!-- Short Names -->
-    <meta name="apple-mobile-web-app-title" content="<?php echo ($applicationName); ?>" />
-    <meta name="application-name" content="<?php echo ($applicationNameShort); ?>" />
+    <meta name="apple-mobile-web-app-title" content="<?php echo $applicationName; ?>" />
+    <meta name="application-name" content="<?php echo $applicationNameShort; ?>" />
 
     <!-- Mobile Manifest -->
     <link rel="manifest" href="manifest.json" />
   <?php } ?>
 
-  <?php if (isset($wallpaper) && !empty($wallpaper) && isset($wallpaperPreload) && $wallpaperPreload === true) { ?>
+  <?php if (
+      isset($wallpaper) &&
+      !empty($wallpaper) &&
+      isset($wallpaperPreload) &&
+      $wallpaperPreload === true
+  ) { ?>
     <link rel="preload" href="<?= $wallpaper ?>" as="image" />
   <?php } ?>
 
   <!-- CSS -->
   <style>
-    <?php require_once($cssUrl); ?>
+    <?php require_once $cssUrl; ?>
   </style>
 
   <!-- Icons -->
@@ -102,13 +122,13 @@ if (empty($linktarget)) {
   <?php if (count($header) > 0) { ?>
     <nav id="header-nav" class="collapse tabs">
       <ul class="collapse-main tablist">
-        <?php $tabnumber = $counterStartvalue ?>
-        <?php foreach ($header as $key) : ?>
+        <?php $tabnumber = $counterStartvalue; ?>
+        <?php foreach ($header as $key): ?>
           <li class="tablist__item<?php if (count($header) == $tabnumber) {
-            echo " tablist__item--last-child";
+              echo " tablist__item--last-child";
           } ?>">
             <a href="#tab-<?= $tabnumber ?>" class="js-tab-trigger" data-target="#tab-<?= $tabnumber ?>"><?= $key ?></a>
-            <?php $tabnumber++ ?>
+            <?php $tabnumber++; ?>
           </li>
         <?php endforeach; ?>
       </ul>
@@ -118,22 +138,39 @@ if (empty($linktarget)) {
 
 <!-- content -->
 <main id="content">
-  <?php $tabnumber = $counterStartvalue ?>
-  <?php foreach ($content as $key) : ?>
+  <?php $tabnumber = $counterStartvalue; ?>
+  <?php foreach ($content as $key): ?>
     <div id="tab-<?= $tabnumber++ ?>" class="tabbed-content">
       <ul class="list-tiles">
-        <?php foreach ($key as $contentItem) : ?>
+        <?php foreach ($key as $contentItem): ?>
           <li class="tile-container">
-            <?php if (isset($contentItem['modal']) && count($contentItem['modal']) > 0) { ?>
-              <a href="<?= $contentItem['url'] ?>" rel="noopener" class="tile js-modal-trigger" data-target="#<?= generateId($contentItem['title']) ?>">
-                <?= returnImage($contentItem['image'], $contentItem['title'],  "tile-image") ?>
-                <span class="tile-title"><?= $contentItem['title'] ?></span>
+            <?php if (
+                isset($contentItem["modal"]) &&
+                count($contentItem["modal"]) > 0
+            ) { ?>
+              <a href="<?= $contentItem[
+                  "url"
+              ] ?>" rel="noopener" class="tile js-modal-trigger" data-target="#<?= generateId(
+    $contentItem["title"]
+) ?>">
+                <?= returnImage(
+                    $contentItem["image"],
+                    $contentItem["title"],
+                    "tile-image"
+                ) ?>
+                <span class="tile-title"><?= $contentItem["title"] ?></span>
               </a>
               <?php renderQrModalTrigger($contentItem); ?>
             <?php } else { ?>
-              <a href="<?= $contentItem['url'] ?>" target="<?= $linktarget ?>" rel="noopener" class="tile">
-                <?= returnImage($contentItem['image'], $contentItem['title'],  "tile-image") ?>
-                <span class="tile-title"><?= $contentItem['title'] ?></span>
+              <a href="<?= $contentItem[
+                  "url"
+              ] ?>" target="<?= $linktarget ?>" rel="noopener" class="tile">
+                <?= returnImage(
+                    $contentItem["image"],
+                    $contentItem["title"],
+                    "tile-image"
+                ) ?>
+                <span class="tile-title"><?= $contentItem["title"] ?></span>
               </a>
               <?php renderQrModalTrigger($contentItem); ?>
             <?php } ?>
@@ -150,13 +187,17 @@ if (empty($linktarget)) {
     <div class="flyout-content">
       <button class="js-flyout-trigger flyout-close" data-target="#bookmarks">&times;</button>
       <h2 class="flyout-title">Bookmarks</h2>
-      <?php foreach ($bookmarks as $key => $contentItems) : ?>
+      <?php foreach ($bookmarks as $key => $contentItems): ?>
         <details>
           <summary><?= $key ?></summary>
           <ul class="list-vertical">
-            <?php foreach ($contentItems as $contentItem) : ?>
+            <?php foreach ($contentItems as $contentItem): ?>
               <li class="list-vertical__item">
-                <a href="<?= $contentItem['url'] ?>" target="<?= $linktarget ?>" class="list-vertical__link"><?= $contentItem['title'] ?></a>
+                <a href="<?= $contentItem[
+                    "url"
+                ] ?>" target="<?= $linktarget ?>" class="list-vertical__link"><?= $contentItem[
+    "title"
+] ?></a>
               </li>
             <?php endforeach; ?>
           </ul>
@@ -168,40 +209,62 @@ if (empty($linktarget)) {
 <?php } ?>
 
 <!-- modals -->
-<?php foreach ($content as $key) : ?>
-  <?php foreach ($key as $contentItem) : ?>
-    <?php if (isset($contentItem['modal']) && count($contentItem['modal']) > 0) { ?>
-      <div id="<?= generateId($contentItem['title']) ?>" class="modal">
+<?php foreach ($content as $key): ?>
+  <?php foreach ($key as $contentItem): ?>
+    <?php if (
+        isset($contentItem["modal"]) &&
+        count($contentItem["modal"]) > 0
+    ) { ?>
+      <div id="<?= generateId($contentItem["title"]) ?>" class="modal">
         <div class="modal-overlay">
           <div class="modal-header">
-            <?= $contentItem['title'] ?>
-            <button class="modal-header__close js-modal-trigger" data-target="#<?= generateId($contentItem['title']) ?>">&times;</button>
+            <?= $contentItem["title"] ?>
+            <button class="modal-header__close js-modal-trigger" data-target="#<?= generateId(
+                $contentItem["title"]
+            ) ?>">&times;</button>
           </div>
           <div class="modal-content">
             <ul class="modal-list">
-              <?php foreach ($contentItem['modal'] as $contentModalLink) : ?>
+              <?php foreach ($contentItem["modal"] as $contentModalLink): ?>
                 <li class="modal-list__item">
-                  <a href="<?= $contentModalLink['url'] ?>" target="<?= $linktarget ?>" class="modal-list__link"><?= $contentModalLink['title'] ?></a>
+                  <a href="<?= $contentModalLink[
+                      "url"
+                  ] ?>" target="<?= $linktarget ?>" class="modal-list__link"><?= $contentModalLink[
+    "title"
+] ?></a>
                 </li>
               <?php endforeach; ?>
             </ul>
           </div><!-- /.modal-content -->
         </div><!-- /.modal-overlay -->
-        <div class="backdrop js-modal-trigger" data-target="#<?= generateId($contentItem['title']) ?>"></div>
+        <div class="backdrop js-modal-trigger" data-target="#<?= generateId(
+            $contentItem["title"]
+        ) ?>"></div>
       </div>
     <?php } ?>
-    <?php if (isset($contentItem['imageQr']) && !empty($contentItem['imageQr'])) { ?>
-      <div id="<?= generateId($contentItem['imageQr']) ?>" class="modal">
+    <?php if (
+        isset($contentItem["imageQr"]) &&
+        !empty($contentItem["imageQr"])
+    ) { ?>
+      <div id="<?= generateId($contentItem["imageQr"]) ?>" class="modal">
         <div class="modal-overlay">
           <div class="modal-header">
-            <?= $contentItem['title'] ?>
-            <button class="modal-header__close js-modal-trigger" data-target="#<?= generateId($contentItem['imageQr']) ?>">&times;</button>
+            <?= $contentItem["title"] ?>
+            <button class="modal-header__close js-modal-trigger" data-target="#<?= generateId(
+                $contentItem["imageQr"]
+            ) ?>">&times;</button>
           </div>
           <div class="modal-content modal-content--qr">
-            <?= returnImage($contentItem['imageQr'], $contentItem['title'],  "tile-image") ?>
+            <?= returnImage(
+                $contentItem["imageQr"],
+                $contentItem["title"],
+                "tile-image"
+            ) ?>
           </div><!-- /.modal-content -->
         </div><!-- /.modal-overlay -->
-        <div class="backdrop js-modal-trigger" data-target="#<?= generateId($contentItem['imageQr']) ?>"></div>
+        <div class="backdrop js-modal-trigger" data-target="#<?= generateId(
+            $contentItem["imageQr"]
+        ) ?>"></div>
       </div>
     <?php } ?>
   <?php endforeach; ?>
@@ -210,13 +273,19 @@ if (empty($linktarget)) {
 <!-- footer -->
 <footer id="application-footer">
   <?php if (count($footer) > 0) { ?>
-    <?php foreach ($footer['description'] as $contentItem) : ?>
-      <div class="description"><a href="<?= $contentItem['url'] ?>" target="<?= $linktarget ?>"><?= $contentItem['title'] ?></a></div>
+    <?php foreach ($footer["description"] as $contentItem): ?>
+      <div class="description"><a href="<?= $contentItem[
+          "url"
+      ] ?>" target="<?= $linktarget ?>"><?= $contentItem["title"] ?></a></div>
     <?php endforeach; ?>
     <div class="social-profiles">
       <ul class="list-horizontal">
-        <?php foreach ($footer['links'] as $contentItem) : ?>
-          <li><a href="<?= $contentItem['url'] ?>" target="<?= $linktarget ?>"><?= $contentItem['title'] ?></a></li>
+        <?php foreach ($footer["links"] as $contentItem): ?>
+          <li><a href="<?= $contentItem[
+              "url"
+          ] ?>" target="<?= $linktarget ?>"><?= $contentItem[
+    "title"
+] ?></a></li>
         <?php endforeach; ?>
       </ul>
     </div>
@@ -233,9 +302,13 @@ press [alt] to open a tab and prevent remembering it
 
 <!-- JS -->
 <script>
-  <?php require_once($jsUrl); ?>
+  <?php require_once $jsUrl; ?>
 
-  <?php if (isset($useServiceWorker) && !empty($useServiceWorker) && ($useServiceWorker == true)) { ?>
+  <?php if (
+      isset($useServiceWorker) &&
+      !empty($useServiceWorker) &&
+      $useServiceWorker == true
+  ) { ?>
   // serviceworker
   navigator.serviceWorker.register("./serviceworker.js", {
     scope: "/"
